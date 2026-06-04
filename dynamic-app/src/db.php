253f143b6ea705d4312f6d874a1dbae8997ba097
icha-aa <sitimalikha.info@gmail.com>
2026-06-05 05:53:25 +0700

    git commit -m "Update database connection settings in db.php"

--- dynamic-app/src/db.php
+++ dynamic-app/src/db.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$db   = 'uas_db';
-$user = 'root';
-$pass = '';
+$host = 'db';
+$db   = 'kerudung_db';
+$user = 'sitimalikha19';
+$pass = 'secretpass';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
